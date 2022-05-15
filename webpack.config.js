0e9ee05ce7dddf96847960ba85b0f2ee50fae728
Anik Hossain <anik.wdev@gmail.com>
2022-05-15 14:41:34 +0600

    need  one more solution to complete

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,29 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const webpackRemoveEmptyScripts = require('webpack-remove-empty-scripts');
+const glob = require('glob');
+
+function entryGenerator(pathName) {
+    return glob.sync(pathName).reduce((entries, entry) => {
+        const entryName = path.parse(entry).name;
+        entries[entryName] = entry;
+        return entries;
+    }, {});
+}
 
 const config = {
     mode: 'development',
-    entry: path.resolve(__dirname, './src/js/noScript.js'),
-    // output: {
-    //     path: path.resolve(__dirname, 'dist/noscript'),
-    //     filename: 'app.js',
-    //     clean: true,
-    // },
+    entry: path.resolve(__dirname, './src/js/app.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js',
+        clean: true,
+    },
 };
 
 const jsConfig = Object.assign({}, config, {
-    entry: {
-        bundle: './src/scripts/index.js',
-        app: './src/scripts/test2.js',
-    },
+    name: 'javaScriptFiles',
+    entry: entryGenerator('./src/js/*'),
     output: {
         path: path.resolve(__dirname, 'dist/js'),
         filename: '[name].[contenthash].js',
@@ -40,40 +47,31 @@ const jsConfig = Object.assign({}, config, {
 });
 
 const htmlConfig = Object.assign({}, config, {
-    // output: {
-    //     path: path.resolve(__dirname, 'dist'),
-    //     filename: '[name].html',
-    //     clean: true,
-    // },
+    name: 'htmlFiles',
     module: {
         rules: [{ test: /\.html$/, use: 'html-loader' }],
     },
     plugins: [
+        new webpackRemoveEmptyScripts({ extensions: /\.js$/ }),
         new htmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
-            publicPath: path.resolve(__dirname, 'dist'),
         }),
         new htmlWebpackPlugin({
             template: './src/about.html',
             filename: 'about.html',
-            publicPath: path.resolve(__dirname, 'dist'),
         }),
         new htmlWebpackPlugin({
             template: './src/contact.html',
             filename: 'contact.html',
-            publicPath: path.resolve(__dirname, 'dist'),
+            chunks: ['about[contenthash].js'],
         }),
     ],
 });
 
 const cssConfig = Object.assign({}, config, {
-    name: 'css',
-    entry: {
-        global: path.resolve(__dirname, 'src/styles/global.scss'),
-        header: path.resolve(__dirname, 'src/styles/header.scss'),
-        app: path.resolve(__dirname, 'src/styles/index.css'),
-    },
+    name: 'cssFiles',
+    entry: entryGenerator('./src/styles/*'),
     output: {
         path: path.resolve(__dirname, 'dist/css'),
         clean: true,
@@ -95,4 +93,24 @@ const cssConfig = Object.assign({}, config, {
     ],
 });
 
-module.exports = [cssConfig, htmlConfig, jsConfig];
+const mediaFilesConfig = Object.assign({}, config, {
+    name: 'MediaFiles',
+    entry: path.resolve(__dirname, './src/scripts/images.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist/images'),
+        clean: true,
+        // filename: '[name].[contenthash].js',
+        assetModuleFilename: '[name][ext]',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|jpeg|png|svg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [new webpackRemoveEmptyScripts({ extensions: /\.js$/ })],
+});
+
+module.exports = [cssConfig, htmlConfig, jsConfig, mediaFilesConfig];
